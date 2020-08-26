7a1bc51890fd6d3f64a4fad0a694a31ffd703aaa
Michal <michal.tyszkiewicz@gmail.com>
2020-08-26 12:23:46 +0200

    Add resizing code

--- warp.py
+++ warp.py
@@ -1,4 +1,5 @@
 import torch, kornia, h5py, imageio
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -46,8 +47,59 @@ def read_data(fname):
 
     return img, depth, K, Rt
 
+def rescale(img: ['H', 'W', 'C'], size) -> ['h', 'w', 'C']:
+    _3d = img.permute(2, 0, 1)
+    _4d = _3d[None, ...]
+    scaled = F.interpolate(
+        _4d,
+        size=size,
+        mode='bilinear',
+        align_corners=False
+    )
+    return scaled.squeeze(0).permute(1, 2, 0)
+
+def pad(img: ['H', 'W', 'C'], size, value=0.) -> ['h', 'w', 'C']:
+    cropped = img[:size[0], :size[1]]
+    # now bitmap cannot be bigger than target size
+
+    xpad = size[0] - cropped.shape[0]
+    ypad = size[1] - cropped.shape[1]
+
+    # not that F.pad takes sizes starting from the last dimension
+    padded = F.pad(cropped, (0, 0, 0, ypad, 0, xpad), mode='constant')
+
+    assert padded.shape[:2] == size
+
+    return padded
+
+def scale_and_pad(img, depth, K, size):
+    '''
+    scale to at most `size` while preserving aspect ratio and then pad
+    the remaining for `img` and `depth`. Adjust `K` accordingly
+    '''
+
+    x_factor = img.shape[0] / size[0]
+    y_factor = img.shape[1] / size[1]
+
+    f = 1 / max(x_factor, y_factor)
+    if x_factor > y_factor:
+        scale_size = (size[0], int(f * img.shape[1]))
+    else:
+        scale_size = (int(f * img.shape[0]), size[1])
+
+    K     = kornia.geometry.epipolar.scale_intrinsics(K, f)
+    img   = pad(rescale(img, scale_size), size)
+    depth = pad(rescale(depth.unsqueeze(-1), scale_size), size).squeeze(-1)
+
+    return img, depth, K
+
 img1, dep1, K1, Rt1 = read_data(fname1)
 img2, dep2, K2, Rt2 = read_data(fname2)
+
+shape = 1024, 1024
+
+img2, dep2, K2 = scale_and_pad(img2, dep2, K2, shape)
+img1, dep1, K1 = scale_and_pad(img1, dep1, K1, shape)
 
 img1_bchw = img1.permute(2, 0, 1)[None]
 img2_bchw = img2.permute(2, 0, 1)[None]
